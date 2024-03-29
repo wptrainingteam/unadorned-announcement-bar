4e18f97b24c83c1cd0b1e397c6932f49855d8600
meszarosrob <hmm@meszarosrob.com>
2024-03-29 17:13:35 +0200

    Tag 1.0.1 version and update the Playground

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: Unadorned Announcement Bar
  * Plugin URI: https://github.com/wptrainingteam/unadorned-announcement-bar
  * Description: A companion plugin for a WordPress Developer Blog article.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires at least: 6.1
  * Requires PHP: 7.4
  * Author: Róbert Mészáros
